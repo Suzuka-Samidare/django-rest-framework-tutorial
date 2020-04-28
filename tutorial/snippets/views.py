9b4d43a1c951a29b2519d7d67ccc4acf073b6565
Suzuka <saberlily031@gmail.com>
2020-04-28 19:44:39 +0900

    [FIX] Django REST framework tutorial 3 mixin実装前まで

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,85 +1,44 @@
-## not
-# from django.http import HttpResponse, JsonResponse
-# from django.views.decorators.csrf import csrf_exempt
-# from rest_framework.parsers import JSONParser
-
-## 第2章で上記の代わりになるもの
+from django.http import Http404
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
-## -----------------------------------------------
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
-## not use
-# @csrf_exempt
-## ------------
-@api_view(['GET', 'POST'])
-def snippet_list(request, format=None):
-    if request.method == 'GET':
+class SnippetList(APIView):
+    def get(self, request, format=None):
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
-        ## not use
-        # return JsonResponse(serializer.data, safe=False)
-        ## ------------------------------------------------
         return Response(serializer.data)
 
-    elif request.method == 'POST':
-        ## not use
-        # data = JSONParser().parse(request)
-        # serializer = SnippetSerializer(data=data)
-        ## -------------------------------------------------
+    def post(self, request, format=None):
         serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            ## not use
-            # return JsonResponse(serializer.data, status=201)
-            ## -----------------------------------------------
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        ## not use
-        # return JsonResponse(serializer.errors, status=400)
-        return Response(serializer.errors, status=sttaus.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-## not use
-# @csrf_exempt
-## ------------
-@api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk, format=None):
-    try:
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        ## not use
-        # return HttpResponse(status=404)
-        ## ----------------------------------------------
-        return Response(status=status.HTTP_404_NOT_FOUND)
-      
-    if request.method == 'GET':
+class SnippetDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet)
-        ## not use
-        # return JsonResponse(serializer.data)
-        ## -----------------------------------------------
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        ## not use
-        # data = JSONParser().parse(request)
-        # serializer = SnippetSerializer(snippet, data=data)
-        ## -------------------------------------------------
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            ## not use
-            # return JsonResponse(serializer.data)
-            ## -------------------------------------------
             return Response(serializer.data)
-        ## not use
-        # return JsonResponse(serializer.errors, status=400)
-        ## -------------------------------------------------
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         snippet.delete()
-        ## not use
-        # return HttpResponse(status=204)
-        ## ------------------------------
         return Response(status=status.HTTP_204_NO_CONTENT)
